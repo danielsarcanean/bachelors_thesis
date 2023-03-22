1866b172d734143b551d29d8b8095f21712a8498
Daniel Sarcanean <115592877+danielsarcanean@users.noreply.github.com>
2023-03-23 00:59:11 +0100

    Simplified functions.py
    Easier input for calibration plotting. Added:
    ```
    process_files(filenames, size_of_files)
    ```
    So every user can insert its location and length of plotting.

--- data/functions.py
+++ data/functions.py
@@ -1,114 +1,91 @@
-import scipy.stats as sp
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import csv
+import matplotlib.pyplot as plt
+import os
+import re
 from scipy import stats
+import textwrap
 
-### FUNCTION FOR GRAPHICAL IMPLEMENTATION ###
+def read_data(file_name):
+    x = []
+    y = []
+    with open(file_name, 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=';')
+        count = 0
+        for row in plots:
+            if count < 3:
+                count += 1
+                continue
+            x.append(float(row[0]))
+            y.append(float(row[1]))
+    return x, y
 
-def plot_graph(file, color):
-  
-  x1 = []
-  x2 = []
-  x3 = []
+def process_files(folder, size):
+    # Creates the containers for each data set
+    # Don't forget to change the concentrations array for the one you will be analyzing!
+    data = []
+    errors = []
+    concentrations = np.array([5, 2.5, 1.25, 0.625, 0.3125, 0.15625, 0.07813, 0.03906, 0.01953, 0.0097, 0])
 
-  y1 = []
-  y2 = []
-  y3 = []
-  with open('20230307/01.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter = ';')
+    for i in range(size+1):
+        # Get a list of all files in the directory that start with {i} and are followed by a non-digit character
+        files = [f for f in os.listdir(folder) if re.match(f'^{i}\\D', f)]
+
+        # Generate x and y values for each file
+        y_values = []
+        for file in files:
+            x, y = read_data(os.path.join(folder, file))
+            y_values.append(y[60])
+
+        # Calculate the mean and standard deviation of the data
+        data_i = np.array(y_values)
+        error_i = data_i.std()
+        data_i = -data_i.mean()
+        data.append(data_i)
+        errors.append(error_i)
+        print(data_i)
     
-    count = 0
+    slope, intercept, r, p, std_err = stats.linregress(concentrations, data)
+
+    def myfunc(x):
+        return slope * x + intercept
+
+    res = list(map(myfunc, concentrations))
     
-    for row in plots:
-        if count < 3:
-            count += 1
-            continue
-        x1.append(float(row[0]))
-        y1.append(float(row[1]))
-with open('20230307/01.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter = ';')
+    # Plot the data
+
+    plt.rcParams["figure.figsize"] = (6,5)
+    plt.rcParams["figure.dpi"] = 300
     
-    count = 0
+    txt = input("Insert the text you want for the figure: ")
+    txt += f" The given linear fit has the form of y = {slope:.3f}x + {intercept:.3f} with a R-squared factor of {r:.3f}. Limit of detection (LOD) at {data[size]+3*errors[size]:.3f} $\mu$A."
+    fig, ax = plt.subplots(figsize=(6,5))
     
-    for row in plots:
-        if count < 3:
-            count += 1
-            continue
-        x1.append(float(row[0]))
-        y1.append(float(row[1]))
-        
-with open('20230307/02.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter = ';')
+    xval = np.linspace(concentrations[-1]-0.1, concentrations[0]+0.1, 5)
+    plt.ylim(data[-1]-20*data[-1], data[0]+0.2)
+    plt.xlim(concentrations[-1]-0.1, concentrations[0]+0.1)
     
-    count = 0
+    # Plot the LOD as a shade that forbids certain data values
+    plt.fill_between(xval, data[-1]-20*data[-1], data[size]+3*errors[size], alpha=0.5, color='orange')
+
+    plt.errorbar(concentrations, data, yerr=errors, fmt='D', capsize=5, color='black', markersize=4)
+    plt.plot(concentrations, res, linestyle=':', color='red')
+    plt.ylabel('Current ($\mu$A)')
+    plt.xlabel("Ferrihexacyanate concentration (mM)")
     
-    for row in plots:
-        if count < 3:
-            count += 1
-            continue
-        x2.append(float(row[0]))
-        y2.append(float(row[1]))
-        
-with open('20230307/03.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter = ';')
-    
-    count = 0
-    
-    for row in plots:
-        if count < 3:
-            count += 1
-            continue
-        x3.append(float(row[0]))
-        y3.append(float(row[1]))
+    wrapped_txt = textwrap.wrap(txt, width=65)  # Wrap text to 50 characters
+    x_pos = -0.1  # Adjust x position of text
+    y_pos = 1.20  # Adjust y position of text
+    for line in wrapped_txt:
+        ax.text(x_pos, y_pos, line, ha='left', va='center', transform=ax.transAxes, fontsize=11)
+        y_pos -= 0.05  # Decrease y position for each line
 
-data0 = np.array([y1[60], y2[60], y3[60]])
-error0 = data0.std()
-data0 = -data0.mean()
+    # Show plot
+    plt.show()
 
+filename = input("Insert the location of your files: ")
+size_of_files = input("Insert the size of the samples: ")
 
-        
-dataw = np.array([y1[60], y2[60], y3[60]])
-errorw = dataw.std()
-dataw = -dataw.mean()
+process_files(filename, size_of_files)
 
-### DATA TREATMENT ###
-
-stacked_data = [dataw, data0, data1, data2, data3, data4, data5, data6, data7, data8, data9]
-stacked_error = [errorw, error0, error1, error2, error3, error4, error5, error6, error7, error8, error9]
-concentrations = [0, 5, 2.5, 1.25, 0.625, 0.3125, 0.15625, 0.07813, 0.03906, 0.01953, 0.0097]
-
-
-slope, intercept, r, p, std_err = stats.linregress(concentrations, stacked_data)
-
-def myfunc(x):
-  return slope * x + intercept
-
-res = list(map(myfunc, concentrations))
-
-### PLOT FORMATTING ###
-
-fig, ax = plt.subplots()
-
-plt.rcParams["figure.figsize"] = (5,5)
-plt.rcParams["figure.dpi"] = 300
-
-xvalues = np.linspace(-5,6,50)
-
-
-
-txt = f"2023/03/07: Chronoamperometric linear fit of the data sets\nwith a curve of y = {slope:.3f}x + {intercept:.3f} and a R-squared factor\nof {r:.3f}, taking the values at 30 s of duration."
-plt.figtext(0, 0.925, txt, wrap=True, horizontalalignment='left', fontsize=11)
-plt.text(1.25, stacked_data[0]*3-(stacked_data[0]*3 + 0.5)/2, f"Limit of detection (LOD) at {stacked_data[0]*3:.3f} $\mu$A", fontsize=7, horizontalalignment='center', verticalalignment='center')
-
-plt.errorbar(concentrations, stacked_data, yerr=stacked_error, linestyle='', marker='D', color='red', markersize=3, capsize=1, barsabove=True, ecolor='black')
-plt.fill_between(xvalues, -1, stacked_data[0]*3, alpha=0.5, color='orange')
-plt.plot(concentrations, res, linestyle=':', color='black')
-plt.ylabel('Current ($\mu A$)')
-plt.xlim(-0.5,5.5)
-plt.ylim(bottom=-0.5)
-plt.xlabel("Ferri's concentration (mM)")
-
-plt.savefig('20230307/calibration_curve_chrono.png')
-plt.show()
